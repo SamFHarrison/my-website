32be2dfd7c3bf853e3a73544ead777a4d2022468
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-07-01 23:24:15 +0100

    feat: moving links under island

--- app/page.tsx
+++ app/page.tsx
@@ -15,76 +15,111 @@ enum PageState {
 
 export default function Home() {
   const [pageState, setPageState] = useState(PageState.LANDING);
+  const [isLinksOpen, setIsLinksOpen] = useState(false);
 
   return (
     <main>
-      <AnimatePresence>
-        {(pageState === PageState.LANDING ||
-          pageState === PageState.WELCOME) && (
-          <motion.div
-            layoutId="island"
-            key="island"
-            className="island"
-            initial={{ borderRadius: "46px" }}
-            exit={{
-              opacity: 0,
-              filter: "blur(5px)",
-              transition: { duration: 0.15 },
-            }}
-          >
-            <motion.button
-              className="avatar"
-              layoutId="avatar"
-              onClick={() => {
-                if (pageState !== PageState.LANDING) {
-                  setPageState(PageState.LANDING);
-                }
+      <div className="container">
+        <AnimatePresence>
+          {(pageState === PageState.LANDING ||
+            pageState === PageState.WELCOME) && (
+            <motion.div
+              layoutId="island"
+              key="island"
+              className="island"
+              initial={{ borderRadius: "46px" }}
+              exit={{
+                opacity: 0,
+                filter: "blur(5px)",
+                transition: { duration: 0.15 },
               }}
             >
-              <Image alt="avatar" src="/assets/avatar.jpg" priority fill />
-            </motion.button>
+              <motion.button
+                className="avatar"
+                layoutId="avatar"
+                onClick={() => {
+                  if (pageState !== PageState.LANDING) {
+                    setPageState(PageState.LANDING);
+                  }
+                }}
+              >
+                <Image alt="avatar" src="/assets/avatar.jpg" priority fill />
+              </motion.button>
 
-            <AnimatePresence mode="popLayout">
-              {pageState === PageState.WELCOME && (
-                <motion.h1
-                  key="title"
-                  layoutId="title"
-                  className="level-3"
-                  initial={{ opacity: 0, filter: "blur(5px)" }}
-                  transition={{ delay: 0.15 }}
-                  animate={{ opacity: 1, filter: "blur(0)" }}
-                  exit={{
-                    opacity: 0,
-                    filter: "blur(5px)",
-                    transition: { duration: 0.15 },
-                  }}
-                >
-                  Hi, I&apos;m <span className="level-1">Sam F-Harrison</span>
-                </motion.h1>
+              <AnimatePresence mode="popLayout">
+                {pageState === PageState.WELCOME && (
+                  <motion.h1
+                    key="title"
+                    layoutId="title"
+                    className="level-3"
+                    initial={{ opacity: 0, filter: "blur(5px)" }}
+                    transition={{ delay: 0.15 }}
+                    animate={{ opacity: 1, filter: "blur(0)" }}
+                    exit={{
+                      opacity: 0,
+                      filter: "blur(5px)",
+                      transition: { duration: 0.15 },
+                    }}
+                  >
+                    Hi, I&apos;m <span className="level-1">Sam F-Harrison</span>
+                  </motion.h1>
+                )}
+              </AnimatePresence>
+
+              {pageState === PageState.LANDING && (
+                <IconButton
+                  icon="add"
+                  onClick={() => setPageState(PageState.WELCOME)}
+                  key="button"
+                  layoutId="button"
+                />
               )}
-            </AnimatePresence>
+              {pageState === PageState.WELCOME && (
+                <IconButton
+                  icon="ellipsis"
+                  onClick={() => setIsLinksOpen(!isLinksOpen)}
+                  key="button"
+                  layoutId="button"
+                />
+              )}
+            </motion.div>
+          )}
 
-            {pageState === PageState.LANDING && (
-              <IconButton
-                icon="add"
-                onClick={() => setPageState(PageState.WELCOME)}
-                key="button"
-                layoutId="button"
-              />
-            )}
-            {pageState === PageState.WELCOME && (
-              <IconButton
-                icon="ellipsis"
-                onClick={() => setPageState(PageState.LINKS)}
-                key="button"
-                layoutId="button"
-              />
-            )}
-          </motion.div>
-        )}
-      </AnimatePresence>
+          {isLinksOpen && (
+            <div className="container link-container">
+              <motion.div
+                layoutId="links"
+                key="links"
+                className="links"
+                initial={{
+                  borderRadius: "46px",
+                  translateY: "-50px",
+                  opacity: 0,
+                  filter: "blur(5px)",
+                }}
+                animate={{
+                  translateY: "0px",
+                  opacity: 100,
+                  filter: "blur(0px)",
+                  transition: { type: "spring", duration: 2 },
+                }}
+                exit={{
+                  translateY: "-50px",
+                  // opacity: 0,
+                  filter: "blur(5px)",
+                  transition: { duration: 0.15 },
+                }}
+              >
+                <motion.button layout className="back-button">
+                  <Icon name="left-arrow" />
+                </motion.button>
+              </motion.div>
+            </div>
+          )}
+        </AnimatePresence>
+      </div>
 
-      <AnimatePresence mode="popLayout">
+      {/* <AnimatePresence mode="popLayout">
         {pageState === PageState.LINKS && (
           <motion.div
             layoutId="island"
@@ -102,7 +137,7 @@ export default function Home() {
             </motion.button>
           </motion.div>
         )}
-      </AnimatePresence>
+      </AnimatePresence> */}
     </main>
   );
 }
